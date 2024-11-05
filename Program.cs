902ee8f30a735dac41edb946c445977c3d6f466b
Beshoy Seleem <bishoy2001@yahoo.com>
2024-11-06 00:01:31 +0200

    First Assignment

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            decimal number1, number2;
+            
+            Console.WriteLine("Type number 1: ");
+            number1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type number 2: ");
+            number2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Sum : {number1 + number2} \n Subtract: {number1 - number2} \n" +
+                              $" Multiply: {number1 * number2}");
+            if (number2 == 0m)
+            {
+                Console.WriteLine("Division : Sorry, can not divide by zero!");
+            }
+            else
+            {
+                Console.WriteLine($"Division: {number1 / number2}");
+            }
+            Console.WriteLine("*************************************************");
+
+            string firstName, LastName;
+            Console.WriteLine("Type First Name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Type Last Name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine($"Your Full Name is: {firstName} {LastName} \n" +
+                              $"The Full Name has {(firstName + LastName).Length} letters");
+            Console.WriteLine("Thanks");
+        }
+    }
+}
