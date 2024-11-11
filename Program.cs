f8f712a7775cceb2d481d5290e8f2c78e0761d2b
Beshoy2001 <bishoy2001@yahoo.com>
2024-11-12 00:39:37 +0200

    Third Assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment1
+﻿using System.ComponentModel;
+
+namespace Assignment1
 {
     internal class Program
     {
@@ -39,63 +41,204 @@
 
             #region Second Assignment
 
-            decimal degree;
-            Console.WriteLine("Type your degree: ");
-            degree = Convert.ToDecimal(Console.ReadLine());
+            //decimal degree;
+            //Console.WriteLine("Type your degree: ");
+            //degree = Convert.ToDecimal(Console.ReadLine());
 
-            if (degree <= 50)
+            //if (degree <= 50)
+            //{
+            //    Console.WriteLine("You FAIL !");
+            //}
+            //else if (degree <= 65)
+            //{
+            //    Console.WriteLine("You get Fair :");
+            //}
+            //else if (degree <= 75)
+            //{
+            //    Console.WriteLine("You are GOOD.");
+            //}
+            //else if (degree <= 85)
+            //{
+            //    Console.WriteLine("You are Very GOOD.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are EXCELLENT.");
+            //}
+            //Console.WriteLine("********************************************************");
+
+            //int choice;
+            //char proceed;
+
+            //do
+            //{
+            //    Console.WriteLine("Choose : \n\n" +
+            //                      "1) Good Morning. \n2) Good Evening. \n3) Good Afternoon. \n4) Good Night.");
+            //    choice = Convert.ToInt32(Console.ReadLine());
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Good Morning.");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Good Evening.");
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Good Afternoon.");
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Good Night.");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid Choice !.");
+            //            break;
+            //    }
+            //    Console.WriteLine("Do you want to continue ? ( y / n ) : ");
+            //    proceed = Convert.ToChar(Console.ReadLine());
+
+            //} while (proceed == 'y');
+            #endregion
+
+            #region Third Assignment
+
+            decimal number1, number2, addition, subtract, multiply, division;
+
+            Console.WriteLine("Type number 1: ");
+            number1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type number 2: ");
+            number2 = Convert.ToDecimal(Console.ReadLine());
+
+            addition = Add(number1, number2);
+            subtract = Subtract(number1, number2);
+            multiply = Multiply(number1, number2);
+
+
+            Console.WriteLine($"Sum : {addition} \nSubtract: {subtract} \n" +
+                              $"Multiply: {multiply}");
+            if (number2 == 0m)
             {
-                Console.WriteLine("You FAIL !");
-            }
-            else if (degree <= 65)
-            {
-                Console.WriteLine("You get Fair :");
-            }
-            else if (degree <= 75)
-            {
-                Console.WriteLine("You are GOOD.");
-            }
-            else if (degree <= 85)
-            {
-                Console.WriteLine("You are Very GOOD.");
+                Console.WriteLine("Division : Sorry, can not divide by zero!");
             }
             else
             {
-                Console.WriteLine("You are EXCELLENT.");
+                division = Divide(number1, number2);
+                Console.WriteLine($"Division: {division}");
             }
-            Console.WriteLine("********************************************************");
+            Console.WriteLine("*************************************************");
 
-            int choice;
-            char proceed;
-           
-            do
+            int size, number, numberOfOcurrence;
+            int[] array;
+
+
+            Console.Write("Type the size of the integer array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            array = new int[size];
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine("Choose : \n\n" +
-                                  "1) Good Morning. \n2) Good Evening. \n3) Good Afternoon. \n4) Good Night.");
-                choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("Good Morning.");
-                        break;
-                    case 2:
-                        Console.WriteLine("Good Evening.");
-                        break;
-                    case 3:
-                        Console.WriteLine("Good Afternoon.");
-                        break;
-                    case 4:
-                        Console.WriteLine("Good Night.");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Choice !.");
-                        break;
-                }
-                Console.WriteLine("Do you want to continue ? ( y / n ) : ");
-                proceed = Convert.ToChar(Console.ReadLine());
+                Console.Write($"Type number {i + 1} : ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Type number to count its occurrence :");
+            number = Convert.ToInt32(Console.ReadLine());
 
-            } while (proceed == 'y');
+            numberOfOcurrence = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (number == array[i])
+                {
+                    numberOfOcurrence++;
+                }
+            }
+            Console.WriteLine($"The number {number} occurred {numberOfOcurrence} time(s);");
+            Console.WriteLine("*****************************************************************");
+
+            int sizeOfArray;
+            int[] arr;
+            Console.Write("Type the size of the integer array to reverse: ");
+            sizeOfArray = Convert.ToInt32(Console.ReadLine());
+            arr = new int[sizeOfArray];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Type number {i + 1} : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int temp = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = arr[i];
+                arr[i] = temp;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Number {i + 1} = {arr[i]}");
+            }
+            Console.WriteLine("*****************************************************************");
+
+            int _sizeOfArray;
+            int[] _arr;
+            bool[] _indexes;
+            Console.Write("Type the size of the integer array to remove duplicates: ");
+            _sizeOfArray = Convert.ToInt32(Console.ReadLine());
+            _arr = new int[_sizeOfArray];
+            _indexes = new bool[_sizeOfArray];
+            
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                Console.Write($"Type number {i + 1} : ");
+                _arr[i] = Convert.ToInt32(Console.ReadLine());
+                _indexes[i] = true;
+            }
+           
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                int temp = _arr[i];
+                
+                for (int j = i + 1; j < _arr.Length; j++)
+                {
+                    if (temp == _arr[j])
+                    {
+                        _indexes[j] = false;
+                    }
+                  
+                }
+            }
+            Console.WriteLine("\nNon Duplicate Numbers are :");
+            for (int i = 0; i < _indexes.Length; i++)
+            {
+                if(_indexes[i] == true)
+                {
+                    Console.WriteLine($"Number {i + 1} : {_arr[i]}");
+                }
+            }
+
+            
             #endregion
         }
+
+        #region Third Assignment
+        private static decimal Add(decimal number1, decimal number2)
+        {
+            decimal result = number1 + number2;
+            return result;
+        }
+        private static decimal Subtract(decimal number1, decimal number2)
+        {
+            decimal result = number1 - number2;
+            return result;
+        }
+        private static decimal Multiply(decimal number1, decimal number2)
+        {
+            decimal result = number1 * number2;
+            return result;
+        }
+        private static decimal Divide(decimal number1, decimal number2)
+        {
+            decimal result = number1 / number2;
+            return result;
+        }
+        #endregion
     }
 }
